314c0202b2feea00c16204d3e5df68308a80c79b
jjfeng <jean.feng@gmail.com>
2020-09-29 14:30:56 -0700

    update stoch

--- code/regret_bounds_restrict_drift_stochastic.py
+++ code/regret_bounds_restrict_drift_stochastic.py
@@ -5,19 +5,21 @@ from scipy.stats import norm
 drift = 0.1
 
 max_loss = 1
+n = 100
 m = 10
 baseline_weight = 1/m
 T = 50
 tau = np.sqrt(np.log(T))
 sigma_max = 0.5
+inflation = tau * sigma_max/np.sqrt(n)
+print("inflation", inflation)
 lambdas = np.exp(np.arange(-6, 2, 0.05))
 deltas = np.arange(0.03, min(max_loss, 0.3), 0.03)
 for delta in deltas:
-    inflation = tau * sigma_max/np.sqrt(n)
-    c = delta + drift + inflation
+    c = min(delta + drift + 2 * inflation, 1)
     bounds = (
         c/ (1 - np.exp(-lambdas * c))
-        * (lambdas * (inflation + delta) * T + np.log(1/baseline_weight))
+        * (np.log(np.exp(-lambdas * delta * T) + (m - 1) * np.exp(-lambdas * c * T)) + np.log(m) + lambdas * inflation * T)
     )/T
     best_bound = np.min(bounds)
     best_lambda = lambdas[np.argmin(bounds)]
