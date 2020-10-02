113b15b9e26d55645916319414ccf91d123876c2
jjfeng <jean.feng@gmail.com>
2020-10-01 17:03:01 -0700

    update regret bounds

--- code/regret_bounds_restrict_drift_stochastic.py
+++ code/regret_bounds_restrict_drift_stochastic.py
@@ -13,21 +13,22 @@ inflation = tau * sigma_max/np.sqrt(n)
 print("inflation", inflation)
 lambdas = np.exp(np.arange(-6, 2, 0.05))
 deltas = np.arange(0.03, min(max_loss, 0.3), 0.03)
+alphas = np.arange(0.5, 1, 0.1)
 index = 130
 for delta in deltas:
     drift = delta
     c = min(delta + drift + 1 * inflation, 1)
-    multiplier = 1/((1 - np.exp(-lambdas * c))/c * (1 - alpha) + (1 - np.exp(-lambdas)) * alpha)
-    raw_bound = (-np.log(np.exp(-lambdas * delta * T) + (m - 1) * np.exp(-lambdas * c * T)) + np.log(m))
+    z_alphas = norm.ppf(alphas)
+    cs = delta + drift + z_alphas * sigma_max/np.sqrt(n)
+    integral = np.array([np.mean((1 - np.exp(-lam * cs))/cs) for lam in lambdas])
+    multiplier = 1/integral
+    raw_bound = -np.log(1/m * np.exp(-lambdas * delta * T) + (m - 1)/m * np.exp(-lambdas * (delta + drift) * T))
     bounds = (
         multiplier * raw_bound
     )/T
     best_bound = np.min(bounds)
     best_idx = np.argmin(bounds)
     best_lambda = lambdas[best_idx]
-    #print(delta, lambdas[index], bounds[index])
-    print(delta, "c", c, (1 - np.exp(-lambdas[best_idx] * c)))
-    print(delta, multiplier[best_idx], raw_bound[best_idx]/T, 1/(1 - np.exp(-lambdas[best_idx])), c/(1 - np.exp(-lambdas[best_idx] * c)))
     # Create the plot
     plt.plot(
         lambdas,
