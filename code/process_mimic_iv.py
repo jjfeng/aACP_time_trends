316652bb58f94eabc2d99e0723a168bf276ead05
jjfeng <jean.feng@gmail.com>
2020-10-06 09:01:57 -0700

    clean up mimic preprocess

--- code/process_mimic_iv.py
+++ code/process_mimic_iv.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import numpy as np
 import pandas as pd
 
@@ -5,74 +7,56 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 
 # items that we want to read
-OUTCOME = "big_los"
+OUTCOME = "survival_status"
 ITEM_IDS = [
     220045,  # Heart rate
     220210,  # Respiratory rate
+    224690,  # Respiratory rate (total)
     220050,  # Arterial Blood Pressure systolic
     220051,  # Arterial Blood Pressure diastolic
     220052,  # Arterial Blood Pressure mean
+    220179,  # Non Invasive Blood Pressure systolic
+    220180,  # Non Invasive Blood Pressure diastolic
+    220181,  # Non Invasive Blood Pressure mean
     223761,  # Temperature Fahrenheit
     220277,  # Oxygen
-    225690,  # Bilirubin
-    220621,  # Glucose
-    220228,  # Hemoglobin
-    220615,  # Creatinine (serum)
-    # 220739, # GCS eye
-    # 223900, # GCSVerbal
-    # 223901, # GCS motor
-    # 227456, # Albumin
-    # 227586
+    # 225690,  # Bilirubin
+    # 220228,  # Hemoglobin
+    # 220615,  # Creatinine (serum)
+    220739,  # GCS eye
+    223900,  # GCSVerbal
+    223901,  # GCS motor
+    225664,  # Glucose finger stick
+    220621,  # Glucose (serum)
+    226537,  # Glucose (whole blood)
 ]
+# LAB_ITEMS = [
+#    51221, # hematocrit
+#    51222, # hemoglobin
+#    51248, # MCH
+#    51249, # MCHC
+#    51250, # MCV
+#    51265, # platelets
+#    51279, # RBC
+#    51277, # RDW
+#    52159, # RDW SD
+#    51301  # WBC
+# ]
+ITEM_ID_STR = "\|".join([str(itemid) for itemid in ITEM_IDS])
 
-patients = pd.read_csv("~/mimic_iv/core/patients.csv.gz")
-patients[["anchor_year_min", "anchor_year_max"]] = patients.anchor_year_group.str.split(
-    " - ", expand=True
-)
-patients["anchor_year_mean"] = (
-    patients.anchor_year_min.astype(int) + patients.anchor_year_max.astype(int)
-) / 2
-print(patients)
-
-admissions = pd.read_csv("~/mimic_iv/core/admissions.csv.gz")
-admissions = admissions.merge(patients, on="subject_id")
-admissions_dt = pd.to_datetime(admissions.admittime).dt
-admissions["in_year"] = (
-    admissions_dt.year - admissions.anchor_year + admissions.anchor_year_mean
-)
-admissions["in_month"] = admissions_dt.month
-admissions_deathtime = pd.to_datetime(admissions.deathtime).dt
-print(pd.to_datetime(admissions.deathtime).dt)
-admissions["survival_status"] = np.isnan(admissions_deathtime.year)
-print(admissions)
+EQUIV_ITEM_IDS = {
+    220179: 220050,
+    220180: 220051,
+    220181: 220052,
+    224690: 220210,
+    225664: 220621,
+    226537: 220621,
+}
 
 
-stays = pd.read_csv("~/mimic_iv/icu/icustays.csv.gz")
-admissions = admissions.merge(stays, on=["subject_id", "hadm_id"])
-admissions["big_los"] = admissions.los > 3
-
-chartevents = pd.read_csv("~/mimic_iv/icu/chartevents.csv.gz", nrows=5000000)
-# chartevents = pd.read_csv("~/mimic_iv/icu/chartevents_small.csv", nrows=10000)
-chartevents = chartevents[chartevents.itemid.isin(ITEM_IDS)]
-chartevents = chartevents.merge(
-    admissions[["subject_id", "hadm_id", "in_year", "in_month", "admittime", OUTCOME]],
-    on=["subject_id", "hadm_id"],
-)
-chartevents["since_admittime"] = pd.to_datetime(chartevents.charttime) - pd.to_datetime(
-    chartevents.admittime
-)
-chartevents["within_24hr"] = chartevents.since_admittime < np.timedelta64(24, "h")
-print(chartevents)
-chartevents = chartevents[chartevents["within_24hr"]]
-
-chartevents.to_csv("~/mimic_iv/test.csv")
-chartevents = pd.read_csv("~/mimic_iv/test.csv")
-
-all_xy_dfs = []
-all_years = []
-for in_year, chartevents_sub in chartevents.groupby(["in_year"]):
-    chartvals = chartevents_sub[["subject_id", "hadm_id", "itemid", "value"]]
-    survs = chartevents_sub[["subject_id", "hadm_id", OUTCOME]].drop_duplicates()
+def create_features(chartevents_sub):
+    chartvals = chartevents_sub[["subject_id", "hadm_id", "itemid", "valuenum"]]
+    print("CAR VALS", chartvals)
     mean_item_values = (
         chartvals.groupby(by=["subject_id", "hadm_id", "itemid"]).mean().reset_index()
     )
@@ -80,7 +64,7 @@ for in_year, chartevents_sub in chartevents.groupby(["in_year"]):
         others=["mean"] * mean_item_values.shape[0], sep=""
     )
     mean_item_values = mean_item_values.pivot(
-        index=["subject_id", "hadm_id"], columns="itemid", values="value"
+        index=["subject_id", "hadm_id"], columns="itemid", values="valuenum"
     )
     print(mean_item_values)
     max_item_values = (
@@ -90,7 +74,7 @@ for in_year, chartevents_sub in chartevents.groupby(["in_year"]):
         others=["max"] * max_item_values.shape[0], sep=""
     )
     max_item_values = max_item_values.pivot(
-        index=["subject_id", "hadm_id"], columns="itemid", values="value"
+        index=["subject_id", "hadm_id"], columns="itemid", values="valuenum"
     )
     min_item_values = (
         chartvals.groupby(by=["subject_id", "hadm_id", "itemid"]).min().reset_index()
@@ -99,42 +83,114 @@ for in_year, chartevents_sub in chartevents.groupby(["in_year"]):
         others=["min"] * min_item_values.shape[0], sep=""
     )
     min_item_values = min_item_values.pivot(
-        index=["subject_id", "hadm_id"], columns="itemid", values="value"
+        index=["subject_id", "hadm_id"], columns="itemid", values="valuenum"
     )
-    variables = pd.concat([mean_item_values, max_item_values, min_item_values])
-    xy_df = survs.merge(variables, on=["subject_id", "hadm_id"])
-    print(in_year, xy_df.shape)
-    all_xy_dfs.append(xy_df)
-    all_years.append(in_year)
-    # xy_df = xy_df.to_numpy()[:,2:]
+    variables = pd.concat([mean_item_values, max_item_values, min_item_values], axis=1)
+    print("VAR", variables)
+    return variables
 
-    # save year month data
-    # np.savetxt("experiment_mimic/_output/data/data_%d.csv" % in_year, xy_df)
+
+def impute_mean(variables):
+    # Fill missing with mean
+    variables = variables.fillna(variables.mean(axis=0, skipna=True))
+    return variables
+
+
+def extract_patients():
+    patients = pd.read_csv("~/mimic_iv/core/patients.csv.gz")
+    # Get the approximate year the patient was first treated
+    patients[
+        ["anchor_year_min", "anchor_year_max"]
+    ] = patients.anchor_year_group.str.split(" - ", expand=True)
+    patients["anchor_year_mean"] = (
+        patients.anchor_year_min.astype(int) + patients.anchor_year_max.astype(int)
+    ) / 2
+    return patients
+
+
+def extract_admissions():
+    admissions = pd.read_csv("~/mimic_iv/core/admissions.csv.gz")
+    admissions = admissions.merge(patients, on="subject_id")
+    admissions_dt = pd.to_datetime(admissions.admittime).dt
+    admissions["in_year"] = (
+        admissions_dt.year - admissions.anchor_year + admissions.anchor_year_mean
+    )
+    admissions["in_month"] = admissions_dt.month
+    admissions_deathtime = pd.to_datetime(admissions.deathtime).dt
+    admissions["survival_status"] = np.isnan(admissions_deathtime.year)
+    return admissions
+
+
+def extract_stays(thres=2):
+    stays = pd.read_csv("~/mimic_iv/icu/icustays.csv.gz")
+    stays["long_stay"] = stays.los > thres
+    return stays
+
+
+def extract_chartevents(nrows=1000000):
+    chartevents = pd.read_csv("~/mimic_iv/icu/chartevents.csv.gz", nrows=nrows)
+    # chartevents = pd.read_csv("~/mimic_iv/icu/chartevents_small.csv", nrows=10000)
+    chartevents = chartevents[chartevents.itemid.isin(ITEM_IDS)]
+    chartevents = chartevents.replace({"itemid": EQUIV_ITEM_IDS})
+    chartevents = chartevents.merge(
+        admissions[["subject_id", "hadm_id", "admittime"]],
+        on=["subject_id", "hadm_id"],
+    )
+    chartevents["since_admittime"] = pd.to_datetime(
+        chartevents.charttime
+    ) - pd.to_datetime(chartevents.admittime)
+    chartevents["within_24hr"] = chartevents.since_admittime < np.timedelta64(24, "h")
+    return chartevents
+
+
+# output = subprocess.check_output(
+#    "zgrep '%s' ~/mimic_iv/icu/chartevents.csv.gz > ~/mimic_iv/icu/chartevents_filtered.csv" % ITEM_ID_STR,
+#    shell=True)
+
+patients = extract_patients()
+admissions = extract_admissions()
+print(admissions)
+stays = extract_stays(thres=2)
+
+# Filter for patients staying at least one day
+stays = stays[stays.los >= 1]
+admissions = admissions.merge(stays, on=["subject_id", "hadm_id"])
+
+chartevents = extract_chartevents(nrows=10000000)
+print(chartevents)
+# Filter for events only within the first 24 hours
+chartevents = chartevents[chartevents["within_24hr"]]
+# Extract X
+features = create_features(chartevents)
+features = impute_mean(features)
+print(features)
+# Merge with Y and year
+full_xy_df = (admissions[["subject_id", "hadm_id", "in_year", OUTCOME]]).merge(
+    features, on=["subject_id", "hadm_id"]
+)
+full_xy_df = full_xy_df.sample(frac=1)
+
+# chartevents.to_csv("~/mimic_iv/test.csv")
+# chartevents = pd.read_csv("~/mimic_iv/test.csv")
+
+all_xy_dfs = []
+for in_year, year_df in full_xy_df.groupby(["in_year"]):
+    print(in_year, year_df.shape)
+    print(year_df)
+    xy = year_df.drop(columns="in_year").to_numpy()[:, 2:]
+    ntrain = int(xy.shape[0] * 3 / 4)
+    xy_train = xy[:ntrain, :]
+    xy_valid = xy[ntrain:, :]
+    np.savetxt("experiment_mimic/_output/data/train_data_%d.csv" % in_year, xy_train)
+    np.savetxt("experiment_mimic/_output/data/valid_data_%d.csv" % in_year, xy_valid)
 
 """
 Just test out a prediction model and see what we get
 """
-all_xy_df = pd.concat(all_xy_dfs)
+all_xy = full_xy_df.drop(columns="in_year").to_numpy()[:, 2:]
 
-# FIll with zeros for fun
-print("MEAN", all_xy_df.mean(axis=0, skipna=True))
-all_xy_df = all_xy_df.fillna(all_xy_df.mean(axis=0, skipna=True))
-
-start_idx = 0
-for subgroup_df, in_year in zip(all_xy_dfs, all_years):
-    xy_df = all_xy_df.iloc[start_idx : start_idx + subgroup_df.shape[0]].to_numpy()[:,2:]
-    ntrain = int(xy_df.shape[0] * 3/4)
-    xy_train_df = xy_df[:ntrain,:] 
-    xy_valid_df = xy_df[ntrain:,:] 
-    start_idx += subgroup_df.shape[0]
-    np.savetxt("experiment_mimic/_output/data/train_data_%d.csv" % in_year, xy_train_df)
-    np.savetxt("experiment_mimic/_output/data/valid_data_%d.csv" % in_year, xy_valid_df)
-
-# Shuffle
-all_xy_df = all_xy_df.sample(frac=1)
-
-x = all_xy_df.to_numpy()[:, 3:]
-y = all_xy_df.to_numpy()[:, 2].astype(int)
+x = all_xy[:, 1:]
+y = all_xy[:, 0].astype(int)
 ntrain = int(y.size * 3 / 4)
 x_train = x[:ntrain]
 y_train = y[:ntrain]
