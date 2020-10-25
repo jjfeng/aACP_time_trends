fix up variance est

code/validation_policies.py
@@ -182,7 +182,7 @@ class ValidationPolicy(Policy):
             / num_batches_list
         )
         var_list = (
-            np.sum(self.var_loss_histories[: time_t + 1, -self.num_back_batches
+            np.mean(self.var_loss_histories[: time_t + 1, -self.num_back_batches
                 :], axis=1)
         )
         batch_sizes = np.array([np.sum(self.batch_sizes[-int(b):]) for b in
@@ -192,6 +192,9 @@ class ValidationPolicy(Policy):
             var_list / batch_sizes
         )
         predictions = mean_loss + inflation
+        print("INF", inflation)
+        print("pre", predictions)
+        print("mean", mean_loss)
         # TODO: Give some reasonable prediction for newest model
         predictions[-1] = predictions[-2]
 