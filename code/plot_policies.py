40ad609d6c5236640d137190ba260256a96e6cff
jjfeng <jean.feng@gmail.com>
2020-11-01 09:37:40 -0800

    update plots

--- code/plot_policies.py
+++ code/plot_policies.py
@@ -26,6 +26,8 @@ def parse_args(args):
     parser.add_argument("--y-max", type=float, default=10)
     parser.add_argument("--y-min", type=float, default=None)
     parser.add_argument("--scale-loss", type=float, default=1)
+    parser.add_argument("--x-start", type=int, default=None)
+    parser.add_argument("--x-skip", type=int, default=None)
     parser.add_argument("--plot-mean", action="store_true")
     parser.add_argument(
         "--history-files", type=str, help="comma separated pickle files"
@@ -48,7 +50,7 @@ def labeler(label):
     return label if label != "ValidationPolicy" else "MarkovHedge"
 
 def plot_losses(approval_history_dict, fig_name, alpha, scale_loss, ymin, ymax,
-        plot_mean, key_order: List[str]):
+        plot_mean, key_order: List[str], x_start: int = None, x_skip: int = None):
     plt.clf()
     raw_ymin = ymax if ymin is None else ymin
     mean_data_frames = []
@@ -65,6 +67,7 @@ def plot_losses(approval_history_dict, fig_name, alpha, scale_loss, ymin, ymax,
                     "Time": np.arange(T - 1),
                     "Loss": running_avg * scale_loss,
                     "Policy": policy_label,
+                    "Policy_type": not policy_label.startswith("Learning-to-Approve"),
                 }
             )
             raw_data = pd.DataFrame(
@@ -72,6 +75,7 @@ def plot_losses(approval_history_dict, fig_name, alpha, scale_loss, ymin, ymax,
                     "Time": np.arange(T - 1),
                     "Loss": loss_history * scale_loss,
                     "Policy": policy_label,
+                    "Policy_type": not policy_label.startswith("Learning-to-Approve"),
                 }
             )
             mean_data_frames.append(mean_data)
@@ -80,17 +84,20 @@ def plot_losses(approval_history_dict, fig_name, alpha, scale_loss, ymin, ymax,
     mean_data = pd.concat(mean_data_frames)
     raw_data = pd.concat(raw_data_frames)
     if plot_mean:
-        sns.lineplot(
+        ax = sns.lineplot(
             x="Time",
             y="Loss",
             hue="Policy",
             data=mean_data,
+            style="Policy_type",
+            legend=False,
         )
     else:
-        sns.lmplot(
+        ax = sns.lmplot(
             x="Time",
             y="Loss",
             hue="Policy",
+            style="Policy_type",
             data=raw_data,
             lowess=True,
             scatter=False,
@@ -99,6 +106,12 @@ def plot_losses(approval_history_dict, fig_name, alpha, scale_loss, ymin, ymax,
     plt.xlabel("Time")
     plt.hlines(y=alpha, xmin=0, xmax=T, colors='black')
     plt.ylim(max(raw_ymin - 0.05, 0) if ymin is None else ymin, ymax)
+    if x_start is not None:
+        tick_locs = np.arange(0, np.max(mean_data.Time), x_skip)
+        plt.xticks(tick_locs,
+                np.arange(x_start, x_start + tick_locs.size))
+    plt.legend([labeler(k) for k in key_order])
+    plt.tight_layout()
     plt.savefig(fig_name)
 
     mean_loss = raw_data.groupby("Policy").mean().reset_index()
@@ -121,6 +134,7 @@ def plot_human_uses(approval_history_dict, fig_name, plot_mean: bool, key_order)
                     "Time": np.arange(T - 1),
                     "prob": np.cumsum(human_history) / np.arange(1, T),
                     "Policy": policy_label,
+                    "Policy_type": not policy_label.startswith("Learning-to-Approve"),
                 }
             ))
             raw_data_frames.append(pd.DataFrame(
@@ -128,30 +142,35 @@ def plot_human_uses(approval_history_dict, fig_name, plot_mean: bool, key_order)
                     "Time": np.arange(T - 1),
                     "prob": human_history,
                     "Policy": policy_label,
+                    "Policy_type": not policy_label.startswith("Learning-to-Approve"),
                 }
             ))
     mean_data = pd.concat(mean_data_frames)
     raw_data = pd.concat(raw_data_frames)
     if plot_mean:
-        sns.lineplot(
+        ax = sns.lineplot(
             x="Time",
             y="prob",
             hue="Policy",
+            style="Policy_type",
             data=mean_data,
+            legend=False,
         )
     else:
-        sns.lmplot(
+        ax = sns.lmplot(
             x="Time",
             y="prob",
             hue="Policy",
             data=raw_data,
             lowess=True,
             scatter=False,
+            legend=False,
         )
     plt.ylim(0, 1)
     plt.ylabel("Fail-safe-prob" if not plot_mean else "Cum avg Fail-safe prob")
     plt.xlabel("Time")
-    plt.legend()
+    plt.legend([labeler(k) for k in key_order])
+    plt.tight_layout()
     plt.savefig(fig_name)
 
 
@@ -163,15 +182,12 @@ def main(args=sys.argv[1:]):
     )
     np.random.seed(args.seed)
 
+    # Load policy histories
     all_approval_histories = [
         pickle_from_file(history_file) for history_file in args.history_files
     ]
     approval_history_dict = {x: [] for x in all_approval_histories[0].keys()}
     approval_history_keys = list(approval_history_dict.keys())
-    ordered_approval_history_keys = (
-            sorted([k for k in approval_history_keys]) if
-    ("Fixed" not in approval_history_keys) else (sorted([k for k in
-        approval_history_keys if k != "Fixed"]) + ["Fixed"]))
     human_max_losses = []
     for curr_approval_hist in all_approval_histories:
         for k in approval_history_dict.keys():
@@ -180,15 +196,25 @@ def main(args=sys.argv[1:]):
     human_max_loss = np.mean(human_max_losses)
     print("HUMAN MAX", human_max_loss)
 
+    # Sort keys
+    sorted_keys = sorted([k for k in approval_history_keys if not
+        k.startswith("Learning-to-Approve")])
+    ordered_approval_history_keys = ["Learning-to-Approve-4",
+            "Learning-to-Approve-15"] + [k for k in sorted_keys if k != "Fixed"]
+    if "Fixed" in sorted_keys:
+        ordered_approval_history_keys.append("Fixed")
+
     plot_losses(
         approval_history_dict,
         args.loss_plot,
         alpha=human_max_loss * args.scale_loss,
         scale_loss=args.scale_loss,
         ymin=args.y_min,
-        ymax=max(human_max_loss + 0.1, args.y_max),
+        ymax=args.y_max,
         plot_mean=args.plot_mean,
         key_order=ordered_approval_history_keys,
+        x_start=args.x_start,
+        x_skip=args.x_skip,
     )
     plot_human_uses(
         approval_history_dict,
