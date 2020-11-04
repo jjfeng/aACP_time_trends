0e91e6869e2518dc3a78f08b6c5924b37ca24d44
jjfeng <jean.feng@gmail.com>
2020-11-04 09:51:05 -0800

    Tentative bounds

--- code/regret_bounds_restrict_drift_stochastic.py
+++ code/regret_bounds_restrict_drift_stochastic.py
@@ -6,31 +6,34 @@ from scipy.stats import norm
 
 def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=50, sigma_max=0.5):
     assert np.isclose(np.sum(meta_weights), 1)
-    print("DRIFT", drift)
     baseline_weight = meta_weights[0]
     factor = (delta + drift)/(1 - np.exp(-lambdas * (delta + drift)))
     raw_bound = -np.log(
         baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * (delta + drift) * T)
     )
-    standard_error = sigma_max / np.sqrt(n) / np.sqrt(T) * 1.9
+    error = sigma_max**2 /2/n * np.power(lambdas, 2) * T
 
-    bounds = raw_bound * factor / T
-    best_idx = np.argmin(bounds + standard_error)
-    print("SE", standard_error, "bound", bounds[best_idx])
-    return bounds + standard_error
+    bounds = (raw_bound + error) * factor / T
+    best_idx = np.argmin(bounds)
+    print("bound", bounds[best_idx])
+    print("INFLA", bounds[best_idx]/delta)
+    print("best lam", lambdas[best_idx])
+    return bounds
 
 
 def main(args=sys.argv[1:]):
     max_loss = 1
-    n = 1000
-    m = 10
+    n = 75
+    m = 15
     T = 50
     lambdas = np.exp(np.arange(-6, 2, 0.05))
     deltas = np.arange(0.03, min(max_loss, 0.2), 0.03)
     index = 130
     alpha = 0.05
     for delta in deltas:
-        drift = delta * 2
+        print("======================")
+        print("%.3f" % delta)
+        drift = delta
         baseline_weight = 1 / m
         meta_weights = np.ones(m)/m
         bounds = get_regret_bounds(meta_weights, T, delta, drift, lambdas)
@@ -38,7 +41,6 @@ def main(args=sys.argv[1:]):
         best_idx = np.argmin(bounds)
         best_lambda = lambdas[best_idx]
 
-        print("%.3f" % delta)
         # Create the plot
         plt.plot(lambdas, bounds, label="d=%.2f" % delta)
 
