4f5c30b9bcdc1efe4d803b5574c9afb126986232
jjfeng <jean.feng@gmail.com>
2020-11-05 08:14:17 -0800

    push updates

--- code/regret_bounds_restrict_drift_stochastic.py
+++ code/regret_bounds_restrict_drift_stochastic.py
@@ -4,14 +4,44 @@ import numpy as np
 from scipy.stats import norm
 
 
-def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=50, sigma_max=0.5):
+#def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=50, sigma_max=0.5):
+#    assert np.isclose(np.sum(meta_weights), 1)
+#    baseline_weight = meta_weights[0]
+#    factor = (delta + drift)/(1 - np.exp(-lambdas * (delta + drift)))
+#    raw_bound = -np.log(
+#        baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * (delta + drift) * T)
+#    )
+#    error = sigma_max**2 /2/n * np.power(lambdas, 2) * T
+#
+#    bounds = (raw_bound + error) * factor / T
+#    best_idx = np.argmin(bounds)
+#    print("bound", bounds[best_idx])
+#    print("INFLA", bounds[best_idx]/delta)
+#    print("best lam", lambdas[best_idx])
+#    return bounds
+
+def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n, alpha, sigma_max=0.5):
     assert np.isclose(np.sum(meta_weights), 1)
     baseline_weight = meta_weights[0]
-    factor = (delta + drift)/(1 - np.exp(-lambdas * (delta + drift)))
+    eps = drift
+    print(delta + drift + eps)
+    factor1 = (1 - np.exp(-lambdas * (delta + drift)))/(delta + drift)
+    factor2 = (1 - np.exp(-lambdas * (delta + drift + eps)))/(delta + drift + eps)
+    factor3 = 1 - np.exp(-lambdas)
+    alpha1 = 1 - alpha
+    alpha3 = alpha/2
+    alpha2 = alpha - alpha3
+    alpha3 = alpha
+    alpha2 = 0
+    print(factor1[50], factor2[50], factor3[50])
+    print(alpha1, alpha2, alpha3)
+    factor = 1/(factor1 * alpha1 + factor2 * alpha2 + factor3 * alpha3)
+
     raw_bound = -np.log(
-        baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * (delta + drift) * T)
+        #baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * T)
+        baseline_weight * np.exp(-lambdas * delta * T)
     )
-    error = sigma_max**2 /2/n * np.power(lambdas, 2) * T
+    error = np.power(lambdas, 2)/8/n * T
 
     bounds = (raw_bound + error) * factor / T
     best_idx = np.argmin(bounds)
@@ -28,15 +58,13 @@ def main(args=sys.argv[1:]):
     T = 50
     lambdas = np.exp(np.arange(-6, 2, 0.05))
     deltas = np.arange(0.03, min(max_loss, 0.2), 0.03)
-    index = 130
-    alpha = 0.05
+    alpha = 0.1
     for delta in deltas:
         print("======================")
         print("%.3f" % delta)
         drift = delta
-        baseline_weight = 1 / m
         meta_weights = np.ones(m)/m
-        bounds = get_regret_bounds(meta_weights, T, delta, drift, lambdas)
+        bounds = get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=n, alpha=alpha)
         best_bound = np.min(bounds)
         best_idx = np.argmin(bounds)
         best_lambda = lambdas[best_idx]
