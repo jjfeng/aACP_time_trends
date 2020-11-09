fixing up variance estimate for meta exp

code/regret_bounds_restrict_drift_stochastic.py
@@ -3,54 +3,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 
+def _get_bernstein_factor(max_val, lambdas):
+    assert np.all(max_val <= 1)
+    return (1 - np.exp(-max_val * lambdas))/max_val
 
-#def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=50, sigma_max=0.5):
-#    assert np.isclose(np.sum(meta_weights), 1)
-#    baseline_weight = meta_weights[0]
-#    factor = (delta + drift)/(1 - np.exp(-lambdas * (delta + drift)))
-#    raw_bound = -np.log(
-#        baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * (delta + drift) * T)
-#    )
-#    error = sigma_max**2 /2/n * np.power(lambdas, 2) * T
-#
-#    bounds = (raw_bound + error) * factor / T
-#    best_idx = np.argmin(bounds)
-#    print("bound", bounds[best_idx])
-#    print("INFLA", bounds[best_idx]/delta)
-#    print("best lam", lambdas[best_idx])
-#    return bounds
-
-def get_regret_bounds(meta_weights, T, delta, drift, lambdas, n, alpha, sigma_max=0.5):
+def get_regret_bounds(meta_weights, T, delta, drift, ni_margin, lambdas, n, alpha, sigma_max=0.5):
     assert np.isclose(np.sum(meta_weights), 1)
-    baseline_weight = meta_weights[0]
-    # TODO: optimize over all possible epsilon values
-    eps = 0.5 * np.sqrt(n)
-    factor1 = (1 - np.exp(-lambdas * (delta + drift)))/(delta + drift)
-    factor2 = (1 - np.exp(-lambdas * (delta + drift +
-        eps * sigma_max/np.sqrt(n))))/(delta + drift + eps * sigma_max/np.sqrt(n))
-    factor3 = 1 - np.exp(-lambdas)
+    lambdas = lambdas.reshape((1,-1))
+    eps = np.exp(np.arange(-3,0,0.02))
+    _alpha3 = T * np.exp(-2 * np.power(eps, 2) * np.power(sigma_max, 2) * n)
+    eps_mask = ((delta + ni_margin + drift + eps * sigma_max) < 1) & (_alpha3 +
+            alpha < 1) 
+    eps = eps[eps_mask].reshape((-1,1))
+
+    factor1 = _get_bernstein_factor(delta + ni_margin + drift, lambdas)
+    factor2 = _get_bernstein_factor(delta + ni_margin + drift + eps *
+        sigma_max, lambdas)
+    factor3 = _get_bernstein_factor(1, lambdas)
     alpha2 = alpha
-    alpha3 = T * np.exp(-2 * np.power(eps, 2) * np.power(sigma_max, 2))
-    print(alpha3)
-    assert alpha3 < 1
+    alpha3 = T * np.exp(-2 * np.power(eps, 2) * np.power(sigma_max, 2) * n)
     alpha1 = 1 - alpha3 - alpha2
-    assert alpha1 > 0
     factor = 1/(factor1 * alpha1 + factor2 * alpha2 + factor3 * alpha3)
 
+    baseline_weight = meta_weights[0]
     raw_bound = -np.log(
         baseline_weight * np.exp(-lambdas * delta * T) + (1 - baseline_weight) * np.exp(-lambdas * T)
     )
     error = np.power(lambdas, 2)/8/n * T
 
     bounds = (raw_bound + error) * factor / T
-    best_idx = np.argmin(bounds)
-    print("bound", bounds[best_idx])
-    print("INFLA", bounds[best_idx]/delta)
-    print("best lam", lambdas[best_idx])
+    best_bound = np.min(bounds)
+    best_idx = np.where(bounds == np.min(bounds))
+    best_eps_idx = best_idx[0][0]
+    best_lambda_idx = best_idx[1][0]
+    print("best eps", eps.flatten()[best_eps_idx], "vs", 1/np.sqrt(n))
+    bounds = np.min(bounds, axis=0)
     return bounds
 
 
 def main(args=sys.argv[1:]):
+    NI_FACTOR = 0.1
     max_loss = 1
     n = 75
     m = 15
@@ -63,7 +55,13 @@ def main(args=sys.argv[1:]):
         print("%.3f" % delta)
         drift = delta
         meta_weights = np.ones(m)/m
-        bounds = get_regret_bounds(meta_weights, T, delta, drift, lambdas, n=n, alpha=alpha)
+        bounds = get_regret_bounds(
+                meta_weights,
+                T,
+                delta,
+                drift,
+                ni_margin=delta * NI_FACTOR,
+                lambdas=lambdas, n=n, alpha=alpha)
         best_bound = np.min(bounds)
         best_idx = np.argmin(bounds)
         best_lambda = lambdas[best_idx]

code/validation_policies.py
@@ -41,11 +41,11 @@ class ValidationPolicy(Policy):
         self.const_baseline_optim_weight = np.ones(1) * const_baseline_weight
 
         self.ci_alpha = ci_alpha
-        self.batch_sizes = []
+        self.batch_sizes = np.array([])
         # print("self.const_baseline_weight", self.const_baseline_weight)
 
-        self.loss_histories = np.zeros((num_experts, 1))
-        self.var_loss_histories = np.zeros((num_experts, 1))
+        self.loss_histories = np.zeros((num_experts, 0))
+        self.var_loss_histories = np.zeros((num_experts,0))
 
         self.num_back_batches = num_back_batches
 
@@ -97,7 +97,9 @@ class ValidationPolicy(Policy):
         self.var_loss_histories = np.concatenate(
             [self.var_loss_histories, var_new_losses], axis=1
         )
-        self.batch_sizes.append(indiv_robot_loss_t.shape[1])
+        assert self.loss_histories.shape == self.var_loss_histories.shape
+        self.batch_sizes = np.concatenate([self.batch_sizes,
+            [indiv_robot_loss_t.shape[1]]])
         v_weights = self.weights * np.exp(-self.etas[0] * model_losses_t)
         # print(self.weights, model_losses_t)
         v_baseline_weights = self.baseline_weights * np.exp(
@@ -178,29 +180,35 @@ class ValidationPolicy(Policy):
             )
             if (time_t > self.num_back_batches)
             else np.concatenate([np.arange(time_t, 0, step=-1), [1]])
-        )
+        ).astype(int)
         mean_loss = (
             np.sum(self.loss_histories[: time_t + 1, -self.num_back_batches :], axis=1)
             / num_batches_list
         )
-        var_list = (
-            np.sum(
-                self.var_loss_histories[: time_t + 1, -self.num_back_batches :], axis=1
-            )
-            / num_batches_list
-        )
+        var_loss_histories = self.var_loss_histories[: time_t + 1, -self.num_back_batches:]
+        raw_mean_vars = var_loss_histories/self.batch_sizes[-self.num_back_batches:]
         batch_sizes = np.array(
-            [np.sum(self.batch_sizes[-int(b) :]) for b in num_batches_list]
+            [np.concatenate([[0] * (min(self.num_back_batches,
+                var_loss_histories.shape[1]) - int(b)),
+                self.batch_sizes[-int(b) :]]) for b in num_batches_list]
         )
+        tot_batch_sizes = np.sum(batch_sizes, axis=1, keepdims=True)
+        var_list = np.sum(raw_mean_vars * np.power(batch_sizes/tot_batch_sizes,
+            2), axis=1)
 
         pred_t_factor = scipy.stats.norm.ppf(1 - self.ci_alpha/mean_loss.size)
-        inflation = pred_t_factor * np.sqrt(var_list / batch_sizes)
-        predictions = mean_loss + inflation
+        pred_t_factor = scipy.stats.norm.ppf(1 - self.ci_alpha)
+        inflation = pred_t_factor * np.sqrt(var_list)
+        # Predictions using LCB??? the mean?
+        predictions = mean_loss #+ inflation
+        # worst case using UCB???
+        worst_case_predictions = mean_loss + inflation
         #print("INF", inflation)
         #print("pre", predictions)
         #print("mean", mean_loss)
         # TODO: Give some reasonable prediction for newest model
         predictions[-1] = predictions[-2]
+        worst_case_predictions[-1] = worst_case_predictions[-2]
 
         all_optim_weights = special.softmax(
             np.concatenate(
@@ -220,7 +228,7 @@ class ValidationPolicy(Policy):
         self.const_baseline_optim_weight = all_optim_weights[-1:]
 
         # Impose constraint
-        self.optim_weights *= predictions <= (self.human_max_loss + self.ni_margin)
+        self.optim_weights *= worst_case_predictions <= (self.human_max_loss + self.ni_margin)
 
     def get_predict_weights(self, time_t: int):
         denom = (
@@ -259,6 +267,7 @@ class MetaExpWeightingList(Policy):
 
         self.eta_indexes = np.arange(len(eta_list))
         print("ETA INDEX", self.eta_indexes)
+        print("BATCHERS BACK", num_back_batches)
         self.policy_dict = {}
         for idx, etas in enumerate(eta_list):
             self.policy_dict[etas] = ValidationPolicy(