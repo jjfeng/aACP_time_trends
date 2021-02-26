b4120cf12653d4ff147443e4aaec554f6ddbac3a
jjfeng <jean.feng@gmail.com>
2021-02-26 08:16:08 -0800

    rename files

--- code/plot_drift_yelp.py
+++ code/plot_drift_yelp.py
@@ -7,23 +7,18 @@ import logging
 import pandas as pd
 import numpy as np
 import json
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def parse_args(args):
     """ parse command line arguments """
 
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument(
-        "--seed",
-        type=int,
-        help="Random number generator seed for replicability",
-        default=0,
-    )
-    parser.add_argument("--train-file", type=str)
-    parser.add_argument("--year", type=int, default=2008)
-    parser.add_argument("--month", type=int, default=1)
-    parser.add_argument("--log-file", type=str, default="_output/model_log.txt")
-    parser.add_argument("--out-file", type=str, default="_output/model.pkl")
+    parser.add_argument("--file-template", type=str)
+    parser.add_argument("--start-year", type=int, default=2008)
+    parser.add_argument("--num-years", type=int, default=11)
+    parser.add_argument("--out-file", type=str, default="_output/drift_yelp.png")
     parser.set_defaults()
     args = parser.parse_args()
 
@@ -32,25 +27,31 @@ def parse_args(args):
 
 def main(args=sys.argv[1:]):
     args = parse_args(args)
-    logging.basicConfig(
-        format="%(message)s", filename=args.log_file, level=logging.DEBUG
+
+    all_stars = []
+    for year in range(args.start_year, args.start_year + args.num_years):
+        for month in range(1, 13):
+            star_file = args.file_template % (year, month)
+            star_df = pd.read_csv(star_file)
+            star_df["idx"] = 12 * star_df.year + star_df.month
+            all_stars.append(star_df)
+    all_stars = pd.concat(all_stars)
+
+    sns.set_context("paper", font_scale=1.5)
+    ax = sns.lineplot(
+        x="idx",
+        y="stars",
+        data=all_stars,
     )
-    print(args)
-    logging.info(args)
-
-    np.random.seed(args.seed)
-
-    num_stars = []
-    with open(args.train_file, "r") as f:
-        for line in f:
-            line_dict = json.loads(line)
-            num_stars.append(line_dict["stars"])
-    dat = pd.DataFrame({
-        "year": [args.year],
-        "month": [args.month],
-        "stars": [np.mean(num_stars)]})
-    dat.to_csv(args.out_file)
-
+    plt.ylabel("Average number of stars")
+    tick_locs = np.arange(np.min(all_stars.idx), np.max(all_stars.idx), 12)
+    plt.xticks(tick_locs, np.arange(args.start_year, args.start_year + tick_locs.size))
+    plt.xlabel("Year")
+    plt.xticks(rotation='vertical')
+    sns.despine()
+    plt.tight_layout()
+    plt.savefig(args.out_file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
